e3efe20ee0f1c24540ae6e4145f82839a67b3a44
NEBULA1978 <rpascualey@gmail.com>
2023-01-30 21:38:28 +0100

    Mostrando archivos estaticos hola.css

--- server.go
+++ server.go
@@ -5,5 +5,10 @@ import (
         )
 
 func main() {
+							//wardamos en variable fs y Definomos con http rl FileServer y le indicamos donde va a find archivos estaticos
+				fs := http.FileServer(http.Dir("./static"))
+				// Le indicamos al servidor http que  cuando le llegen peticiones en /static utilice  http.StripPrefix("/static/", fs))
+				http.Handle("/static/", http.StripPrefix("/static/", fs))
+
         http.ListenAndServe(":8080", nil)
         }
